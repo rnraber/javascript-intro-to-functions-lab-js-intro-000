66e529e204be74ed145c12765e79decd95fe0d62
Ryann Raber <ryann.raber@yahoo.com>
2019-10-30 22:46:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
+
+function logShout(string) {
+  console.log("HELLO");
+}
+
+function logWhisper(string) {
+  console.log("hello");
+}
